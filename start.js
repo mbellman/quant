e145aa198f1799e286829c5735707300cc922e5e
unknown <mbellman@nwe.com>
2022-07-05 16:55:32 -0500

    more heroku accommodation stuff

--- start.js
+++ start.js
@@ -1,4 +1,26 @@
-const { exec } = require('child_process');
-const webpack = exec('webpack-dev-server --config webpack.config.js', console.log);
+const express = require('express');
+const app = express();
 
-webpack.stdout.on('data', console.log);
+app.listen(process.env.PORT || 1234);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/dist/client/index.html');
+});
+
+app.get('/api/:symbol/:type', async (req, res) => {
+  const { getSymbolData } = require('./dist/server');
+  const data = await getSymbolData(req.params);
+
+  res.send(data);
+});
+
+app.get('/api/random', async (req, res) => {
+  const { getRandomDay } = require('./dist/server');
+  const data = await getRandomDay();
+
+  res.send(data);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/dist/client/' + req.originalUrl);
+});
